de56578ec5c5d5d323dccba2fc8674adb2df86bb
Facundo Pompa <pompafacundo@gmail.com>
2022-12-29 13:32:34 -0300

    first commit

---
+++ src/components/data/stock.js
@@ -0,0 +1,62 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Takagi, la maestra de las bromas #13",
+        tipo: "b5",
+        editorial: "Panini",
+        precio: 1100,
+        img: '/public/images/takagi-13.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Paradise Kiss #4",
+        tipo: "Tanko",
+        editorial: "Ivrea",
+        precio: 1450,
+        img: '/public/images/paradise-kiss-3.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Solo Leveling #2",
+        tipo: "Tanko a color",
+        editorial: "Ivrea",
+        precio: 2950,
+        img: '/public/images/solo-leveling-2.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Amor, Devoraré tu Corazón",
+        tipo: "b6 doble",
+        editorial: "Ivrea",
+        precio: 850,
+        img: '/public/images/amordevoraretucorazon1.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Look Back",
+        tipo: "b5",
+        editorial: "Ivrea",
+        precio: 750,
+        img: '/public/images/look-back.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Dr. Stone #18",
+        tipo: "b5",
+        editorial: "Panini",
+        precio: 1100,
+        img: '/public/images/drstone18.jpg',
+        cantidad:1
+    }
+];
